1e2c7fded1855906c89e678b558bbf05947b3dd5
unknown <deepaksinghgusain>
2022-11-02 17:02:15 +0530

    02-11-2022

---
+++ core-php/1. Basic.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * 1. PHP tags
+ * 2. Escaping from HTML
+ * 3. Instruction separation
+ * 4. Comments
+ * 5. outputing data 
+*/
+
+/**
+ * 
+ * 1. Basics
+ * 2. Predefined Variables
+ * 3. Variable scope
+ * 4. Variable variables 
+ * 5. Variables From External Sources
+ */
+
+/**
+ * 1. Booleans
+ * 2. Integers 
+ * 3. Floating point numbers
+ * 4. Strings 
+ * 5. Numeric strings
+ * 6. Arrays 
+ * 8. Objects 
+ * 9. Enumerations
+ * 10. Resources 
+ * 11. NULL 
+ * 12. Callbacks / Callables
+ * 13. Type declarations
+ * 14. Type Juggling
+ * 15. gettype()
+ */
+
+ 
